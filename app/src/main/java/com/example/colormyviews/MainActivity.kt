cac09156b368a16080cfcf3aab9c192336334745
hesheng1910 <56876037+hesheng1910@users.noreply.github.com>
2021-04-21 16:55:15 +0700

    Fix Error

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.colormyviews
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,13 +21,15 @@ class MainActivity : AppCompatActivity() {
     private fun setListeners() {
         val clickableViews: List<View> =
             listOf(box_one_text, box_two_text, box_three_text,
-                box_four_text, box_five_text)
+                box_four_text, box_five_text, red_button,yellow_button,green_button)
 
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(item) }
         }
     }
+    @SuppressLint("ResourceAsColor")
     private fun makeColored(view: View) {
+
         when (view.id) {
 
             R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
